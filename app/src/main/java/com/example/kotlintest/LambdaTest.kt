432bda6145d051aab0a6b9795114711cda485fef
luoyi <1978640054@qq.com>
2023-09-11 18:06:17 +0800

    fea:更至lambda模块

---
+++ app/src/main/java/com/example/kotlintest/LambdaTest.kt
@@ -0,0 +1,16 @@
+package com.example.kotlintest
+
+class LambdaTest {
+
+    /**
+     * Java写法
+     */
+    fun test() {
+        val list = ArrayList<String>()
+        list.add("Apple")
+        list.add("Banana")
+        list.add("Orange")
+        list.add("Pear")
+        list.add("Grape")
+    }
+}
--- app/src/main/java/com/example/kotlintest/Singleton.kt
+++ app/src/main/java/com/example/kotlintest/Singleton.kt
@@ -3,7 +3,6 @@ package com.example.kotlintest
 /**
  * Kotlin中我们不需要私有化构造函数，也不需要提供getInstance()这样的静态方法，只需要把class关键字改成object关键字，一个单例类就创建完成了
  */
-
 object Singleton {
     fun singletonTest() {
         println("singletonTest is called.")
